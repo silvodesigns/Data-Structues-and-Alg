bd8c2c8c8ee3fa1f36d22a426b5ff18901eb215a
Kevin Silvestre <kevsilvo@gmail.com>
2021-02-03 19:26:52 -0500

    Build Stack

---
+++ Stack.py
@@ -0,0 +1,11 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+        
+    def push(self, item):
+        self.data.append(item)
+        
+    def pop(self):
+        if len(self.data) > 0:
+            return self.data.pop()
+        return "The stack is empty"
